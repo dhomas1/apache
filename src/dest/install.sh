3fa76f454a44b469eb7814a27594a9e793ae6487
Ricardo Padilha <ricardospadilha@gmail.com>
2015-05-06 14:07:01 -0700

    Shellchecked install.sh

--- src/dest/install.sh
+++ src/dest/install.sh
@@ -20,7 +20,7 @@ set -o xtrace   # enable script tracing
 
 # copy default configuration files
 find "${prog_dir}" -type f -name "*.default" -print | while read deffile; do
-  basefile="$(dirname ${deffile})/$(basename ${deffile} .default)"
+  basefile="$(dirname "${deffile}")/$(basename "${deffile}" .default)"
   if [ ! -f "${basefile}" ]; then
     cp -vf "${deffile}" "${basefile}"
   fi
